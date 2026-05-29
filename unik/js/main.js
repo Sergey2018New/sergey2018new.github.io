87a17145546408158cfb0180fb4d36d935cc25eb
Sergei Dudin <ya.sergej20142014@yandex.ru>
2026-05-29 14:39:29 +0500

    update unik

--- unik/js/main.js
+++ unik/js/main.js
@@ -9509,6 +9509,193 @@ function Scrollbar({
   });
 }
 
+/* eslint no-bitwise: ["error", { "allow": [">>"] }] */
+function Controller({
+  swiper,
+  extendParams,
+  on
+}) {
+  extendParams({
+    controller: {
+      control: undefined,
+      inverse: false,
+      by: 'slide' // or 'container'
+    }
+  });
+
+  swiper.controller = {
+    control: undefined
+  };
+  function LinearSpline(x, y) {
+    const binarySearch = function search() {
+      let maxIndex;
+      let minIndex;
+      let guess;
+      return (array, val) => {
+        minIndex = -1;
+        maxIndex = array.length;
+        while (maxIndex - minIndex > 1) {
+          guess = maxIndex + minIndex >> 1;
+          if (array[guess] <= val) {
+            minIndex = guess;
+          } else {
+            maxIndex = guess;
+          }
+        }
+        return maxIndex;
+      };
+    }();
+    this.x = x;
+    this.y = y;
+    this.lastIndex = x.length - 1;
+    // Given an x value (x2), return the expected y2 value:
+    // (x1,y1) is the known point before given value,
+    // (x3,y3) is the known point after given value.
+    let i1;
+    let i3;
+    this.interpolate = function interpolate(x2) {
+      if (!x2) return 0;
+
+      // Get the indexes of x1 and x3 (the array indexes before and after given x2):
+      i3 = binarySearch(this.x, x2);
+      i1 = i3 - 1;
+
+      // We have our indexes i1 & i3, so we can calculate already:
+      // y2 := ((x2−x1) × (y3−y1)) ÷ (x3−x1) + y1
+      return (x2 - this.x[i1]) * (this.y[i3] - this.y[i1]) / (this.x[i3] - this.x[i1]) + this.y[i1];
+    };
+    return this;
+  }
+  function getInterpolateFunction(c) {
+    swiper.controller.spline = swiper.params.loop ? new LinearSpline(swiper.slidesGrid, c.slidesGrid) : new LinearSpline(swiper.snapGrid, c.snapGrid);
+  }
+  function setTranslate(_t, byController) {
+    const controlled = swiper.controller.control;
+    let multiplier;
+    let controlledTranslate;
+    const Swiper = swiper.constructor;
+    function setControlledTranslate(c) {
+      if (c.destroyed) return;
+
+      // this will create an Interpolate function based on the snapGrids
+      // x is the Grid of the scrolled scroller and y will be the controlled scroller
+      // it makes sense to create this only once and recall it for the interpolation
+      // the function does a lot of value caching for performance
+      const translate = swiper.rtlTranslate ? -swiper.translate : swiper.translate;
+      if (swiper.params.controller.by === 'slide') {
+        getInterpolateFunction(c);
+        // i am not sure why the values have to be multiplicated this way, tried to invert the snapGrid
+        // but it did not work out
+        controlledTranslate = -swiper.controller.spline.interpolate(-translate);
+      }
+      if (!controlledTranslate || swiper.params.controller.by === 'container') {
+        multiplier = (c.maxTranslate() - c.minTranslate()) / (swiper.maxTranslate() - swiper.minTranslate());
+        if (Number.isNaN(multiplier) || !Number.isFinite(multiplier)) {
+          multiplier = 1;
+        }
+        controlledTranslate = (translate - swiper.minTranslate()) * multiplier + c.minTranslate();
+      }
+      if (swiper.params.controller.inverse) {
+        controlledTranslate = c.maxTranslate() - controlledTranslate;
+      }
+      c.updateProgress(controlledTranslate);
+      c.setTranslate(controlledTranslate, swiper);
+      c.updateActiveIndex();
+      c.updateSlidesClasses();
+    }
+    if (Array.isArray(controlled)) {
+      for (let i = 0; i < controlled.length; i += 1) {
+        if (controlled[i] !== byController && controlled[i] instanceof Swiper) {
+          setControlledTranslate(controlled[i]);
+        }
+      }
+    } else if (controlled instanceof Swiper && byController !== controlled) {
+      setControlledTranslate(controlled);
+    }
+  }
+  function setTransition(duration, byController) {
+    const Swiper = swiper.constructor;
+    const controlled = swiper.controller.control;
+    let i;
+    function setControlledTransition(c) {
+      if (c.destroyed) return;
+      c.setTransition(duration, swiper);
+      if (duration !== 0) {
+        c.transitionStart();
+        if (c.params.autoHeight) {
+          nextTick(() => {
+            c.updateAutoHeight();
+          });
+        }
+        elementTransitionEnd(c.wrapperEl, () => {
+          if (!controlled) return;
+          c.transitionEnd();
+        });
+      }
+    }
+    if (Array.isArray(controlled)) {
+      for (i = 0; i < controlled.length; i += 1) {
+        if (controlled[i] !== byController && controlled[i] instanceof Swiper) {
+          setControlledTransition(controlled[i]);
+        }
+      }
+    } else if (controlled instanceof Swiper && byController !== controlled) {
+      setControlledTransition(controlled);
+    }
+  }
+  function removeSpline() {
+    if (!swiper.controller.control) return;
+    if (swiper.controller.spline) {
+      swiper.controller.spline = undefined;
+      delete swiper.controller.spline;
+    }
+  }
+  on('beforeInit', () => {
+    if (typeof window !== 'undefined' && (
+    // eslint-disable-line
+    typeof swiper.params.controller.control === 'string' || swiper.params.controller.control instanceof HTMLElement)) {
+      const controlElements = typeof swiper.params.controller.control === 'string' ? [...document.querySelectorAll(swiper.params.controller.control)] : [swiper.params.controller.control];
+      controlElements.forEach(controlElement => {
+        if (!swiper.controller.control) swiper.controller.control = [];
+        if (controlElement && controlElement.swiper) {
+          swiper.controller.control.push(controlElement.swiper);
+        } else if (controlElement) {
+          const eventName = `${swiper.params.eventsPrefix}init`;
+          const onControllerSwiper = e => {
+            swiper.controller.control.push(e.detail[0]);
+            swiper.update();
+            controlElement.removeEventListener(eventName, onControllerSwiper);
+          };
+          controlElement.addEventListener(eventName, onControllerSwiper);
+        }
+      });
+      return;
+    }
+    swiper.controller.control = swiper.params.controller.control;
+  });
+  on('update', () => {
+    removeSpline();
+  });
+  on('resize', () => {
+    removeSpline();
+  });
+  on('observerUpdate', () => {
+    removeSpline();
+  });
+  on('setTranslate', (_s, translate, byController) => {
+    if (!swiper.controller.control || swiper.controller.control.destroyed) return;
+    swiper.controller.setTranslate(translate, byController);
+  });
+  on('setTransition', (_s, duration, byController) => {
+    if (!swiper.controller.control || swiper.controller.control.destroyed) return;
+    swiper.controller.setTransition(duration, byController);
+  });
+  Object.assign(swiper.controller, {
+    setTranslate,
+    setTransition
+  });
+}
+
 /* eslint no-underscore-dangle: "off" */
 /* eslint no-use-before-define: "off" */
 function Autoplay({
@@ -10418,39 +10605,105 @@ function EffectFade({
 
 function initSwiper() {
 
-    const homeSlider = document.querySelector('.js-home-swiper:not(.swiper-initialized)');
+    const homeContentSlider = document.querySelector('.js-home-content-swiper:not(.swiper-initialized)');
+const homeImageSlider = document.querySelector('.js-home-image-swiper:not(.swiper-initialized)');
 
-    if (homeSlider) {
+if (homeContentSlider && homeImageSlider) {
+    // Слайдер с контентом (эффект fade)
+    const contentSwiper = new Swiper('.js-home-content-swiper:not(.swiper-initialized)', {
+        modules: [Keyboard, Navigation, Pagination, EffectFade, Autoplay, Controller],
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+        },
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
+        speed: 800,
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        // Отключаем свайпы на контент-слайдере
 
-        new Swiper('.js-home-swiper:not(.swiper-initialized)', {
-            modules: [Keyboard, Navigation, Pagination, EffectFade, Autoplay ],
-            keyboard: {
-                enabled: true,
-                onlyInViewport: true,
-            },
-            // effect: 'fade',
-            // fadeEffect: {
-            //     crossFade: true
-            // },
-            speed: 800,
-            slidesPerView: 1,
-            spaceBetween: 0,
-            loop: true,
-            navigation: {
-              prevEl: '.js-home-swiper-prev',
-              nextEl: '.js-home-swiper-next',
-            },
-            pagination: {
-                el: '.js-home-swiper-pagination',
-                type: 'bullets',
-                clickable: true,
-            },
-            autoplay: {
-                delay: 5000,
-                disableOnInteraction: false
-            },
-        });
-    }
+        navigation: {
+            prevEl: '.js-home-swiper-prev',
+            nextEl: '.js-home-swiper-next',
+        },
+        pagination: {
+            el: '.js-home-swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+
+    });
+
+    // Слайдер с изображениями (обычный слайд)
+    const imageSwiper = new Swiper('.js-home-image-swiper:not(.swiper-initialized)', {
+        modules: [Keyboard, Navigation, Pagination, EffectFade, Controller],
+        allowTouchMove: false,
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+        },
+         allowTouchMove: false,
+        speed: 800,
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+
+        // autoplay: {
+        //     delay: 5000,
+        //     disableOnInteraction: false
+        // },
+
+    });
+
+    // Синхронизация слайдеров
+    contentSwiper.controller.control = imageSwiper;
+    // imageSwiper.controller.control = contentSwiper;
+
+    // // Синхронизация кнопок навигации
+    // const prevBtn = homeContentSlider.querySelector('.js-home-swiper-prev');
+    // const nextBtn = homeContentSlider.querySelector('.js-home-swiper-next');
+
+    // if (prevBtn && nextBtn) {
+    //     prevBtn.addEventListener('click', () => {
+    //         contentSwiper.slidePrev();
+    //         imageSwiper.slidePrev();
+    //     });
+
+    //     nextBtn.addEventListener('click', () => {
+    //         contentSwiper.slideNext();
+    //         imageSwiper.slideNext();
+    //     });
+    // }
+
+    // // Синхронизация пагинации
+    // const paginationEl = homeContentSlider.querySelector('.js-home-swiper-pagination');
+    // if (paginationEl) {
+    //     // Обновляем активный bullet при смене слайда
+    //     const updatePagination = () => {
+    //         const activeIndex = imageSwiper.realIndex;
+    //         const bullets = paginationEl.querySelectorAll('.swiper-pagination-bullet');
+    //         bullets.forEach((bullet, index) => {
+    //             if (index === activeIndex) {
+    //                 bullet.classList.add('swiper-pagination-bullet-active');
+    //             } else {
+    //                 bullet.classList.remove('swiper-pagination-bullet-active');
+    //             }
+    //         });
+    //     };
+
+    //     imageSwiper.on('slideChange', updatePagination);
+    //     contentSwiper.on('slideChange', updatePagination);
+    //     updatePagination();
+    // }
+}
 
     if (document.querySelector('.js-benefits-swiper:not(.swiper-initialized)')) {
         let benefitsSwiperInstance;
@@ -10917,10 +11170,16 @@ function initSwiper() {
                     productThumbsSwiper = new Swiper(productThumbsSwiperEl, {
                         direction: 'horizontal',
                         modules: [Navigation, Thumb, Mousewheel],
-                        speed: 300,
+                        speed: 200,
                         slidesPerView: 3,
                         spaceBetween: 12,
-                        mousewheel: true,
+                        mousewheel: {
+                            enabled: true,
+                            sensitivity: 2,
+                            //  thresholdDelta: 20,
+                            //  forceToAxis: true,
+                        },
+                          preventInteractionOnTransition: false,
                         loop: false,
                         navigation: {
                             prevEl: '.js-product-slider-thumbs-prev',
@@ -10940,7 +11199,7 @@ function initSwiper() {
                             },
                             1025: {
                                 slidesPerView: 'auto',
-                                slidesPerGroup: 'auto',
+                                // slidesPerGroup: 'auto',
                                 direction: direction,
                                 spaceBetween: 12,
 
@@ -10951,7 +11210,7 @@ function initSwiper() {
                     productThumbsSwiperEl.addEventListener('click', (event) => {
                         const {target} = event;
 
-                        const swiperSlideVisible = target.closest('.swiper-slide-visible');
+                        const swiperSlideVisible = target.classList.contains('swiper-slide-visible') ? target : target.closest('.swiper-slide-visible');
                         const prevSlide = swiperSlideVisible?.previousElementSibling;
                         const nextSlide = swiperSlideVisible?.nextElementSibling;
 
